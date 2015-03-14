4b6f2e390293b8edcfadb435643ca9d190f50325
azieel <faure.julien5214@gmail.com>
2015-03-14 15:51:11 +0100

    Recupere le modele user en meme temps que l'admin dans le serializer admin coté rails

--- app/serializers/admin_serializer.rb
+++ app/serializers/admin_serializer.rb
@@ -1,8 +1,12 @@
 class AdminSerializer < ActiveModel::Serializer
-  attributes :id, :username, :first_name, :last_name, :address, :zip_code, :city, :country, :tel
+  attributes :id, :username, :first_name, :last_name, :address, :zip_code, :city, :country, :tel, :user
 
   def username
     object.username
+  end
+
+  def user
+    object.user
   end
 
 end
